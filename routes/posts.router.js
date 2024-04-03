5a6820415d36fe505a6e9b0d8087b1a492018f20
“JHuang19960705 <abc850705@gmail.com>
2024-04-03 21:30:16 +0800

    部屬

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -8,6 +8,12 @@ router.get("/", (req, res) => {
   const indexPath = path.join(__dirname, '../public/index.html');
   res.sendFile(indexPath); // 將 HTML 檔案發送到客戶端
 });
+
+// 加入 express.static 中介軟體來提供靜態檔案的服務
+router.use("/styles", express.static(path.join(__dirname, '../public/styles')));
+router.use("/scripts", express.static(path.join(__dirname, '../public/scripts')));
+
+
 router.get("/get_todoList", postsController.getAll)
 router.post("/add_todoList", postsController.create)
 router.patch("/update_todoList/:id", postsController.update)
