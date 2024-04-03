df5c299a3a8085d73ed4036c6879b1e4df52bb05
“JHuang19960705 <abc850705@gmail.com>
2024-04-03 21:00:58 +0800

    部屬

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const path = require('path')
 const router = express.Router()
 
 const postsController = require("../controller/posts.controller")
 
 router.get("/", (req, res) => {
-  res.sendFile(__dirname + '/public/index.html'); // 將 HTML 檔案發送到客戶端
+  const indexPath = path.join(__dirname, '../public/index.html');
+  res.sendFile(indexPath); // 將 HTML 檔案發送到客戶端
 });
 router.get("/get_todoList", postsController.getAll)
 router.post("/add_todoList", postsController.create)
